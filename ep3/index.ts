5fbf39e98bc7061fba9bcaea24c7079e2c9fd88b
artArthur021 <wafoth@gmail.com>
2024-07-19 20:46:43 +0900

    function

---
+++ ep3/index.ts
@@ -0,0 +1,53 @@
+function helloWorld () {
+ console.log ("สวัดดี")
+
+}
+
+helloWorld ()
+
+function helloName(name:string) {
+ console.log(name)
+
+}
+helloName('sorn')
+
+function getPi() {
+ return 3.14
+
+}
+
+  console.log (getPi())
+
+
+
+
+ function st (fname :string,sname:string) {
+ 
+  if(fname ==='mix'&& sname==='zane') {
+    console.log('เริ่มสอน')
+
+  } else { 
+    console.log('ไม่สอน')
+
+  }
+}
+ st ('max','zane')
+
+
+
+ function hp (mname: string, zname: string, Pname: string) {
+
+    if(mname ==='mix'&& zname === 'zane' || Pname === 'forth' ) {
+     console.log ('พร้อมเรียน')  
+} else {
+
+     console.log('ยังไม่พร้อม')
+}
+ } hp('mix','zane','forth')
+
+
+
+
+
+ 
+
