6db75790628b6ef961cdbbaaecf79afb438e46a6
artArthur021 <wafoth@gmail.com>
2024-07-19 21:37:09 +0900

    function

--- ep3/index.ts
+++ ep3/index.ts
@@ -24,6 +24,7 @@ function getPi() {
  function st (fname :string,sname:string) {
  
   if(fname ==='mix'&& sname==='zane') {
+
     console.log('เริ่มสอน')
 
   } else { 
@@ -39,11 +40,48 @@ function getPi() {
 
     if(mname ==='mix'&& zname === 'zane' || Pname === 'forth' ) {
      console.log ('พร้อมเรียน')  
+
 } else {
 
      console.log('ยังไม่พร้อม')
 }
  } hp('mix','zane','forth')
+
+
+
+
+
+function soldier (gender:string,height:number,weight:number) {
+    if(gender ==='man'&& height >170 || weight>50 && weight <= 110){
+    console.log('จับใบดำใบแดง')
+    
+    }else {
+    console.log ('ไม่เข้าเกณฑ์')
+    
+}
+}
+soldier('man',170,110)
+
+
+
+
+
+
+
+function Wallet  (Age:number,salary:number,Total:number) {
+   if(Age >= 16 && salary < 70000 && Total < 500000){
+    console.log('รับ10000') 
+
+}else {
+    console.log('อด...')
+
+}
+
+}
+
+Wallet (16,70000,500000)
+
+
 
 
 
