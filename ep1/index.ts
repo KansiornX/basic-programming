3c15358480a1a0d7934f4519f42b0b2bd2b741fa
KansiornX <wafoth@gmail.com>
2024-07-19 16:21:41 +0700

    ส่งงาน2

--- ep1/index.ts
+++ ep1/index.ts
@@ -6,3 +6,11 @@ let อยากกลับบ้านมั้ย:boolean = false
 console.log(firstName)
 console.log(Y)
 console.log(อยากกลับบ้านมั้ย)
+
+
+const a:number = 29
+const b: number = 10
+
+console.log (a !== b)
+console.log (a!==b)
+console.log (a>=b)
