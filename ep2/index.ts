51bef4fc9f42a3efce474f4b105b2992a2bca1f1
Nusuno <wafoth@gmail.com>
2024-07-23 16:03:07 +0700

    เรียน Funtion

---
+++ ep2/index.ts
@@ -0,0 +1,59 @@
+ function add (a:number,b:number) {
+    return a + b 
+ }
+
+
+
+ console.log(add(5,8))
+
+
+
+
+
+
+function subtract (a: number,b:number) {
+  return a-b
+
+}
+
+console.log(subtract(50,25))
+
+
+
+function multiply (a:number,b:number,c:number,d:number) {
+   return a*b*c*d
+
+}console.log(multiply(50,20,30,40))
+
+
+
+
+
+function grade (homework:number,midterm:number,final:number) {
+    let score: number = homework+midterm+final
+    if(score<50) {
+        return 'เกรด F'
+    }else if(score<60) {
+        return 'เกรด D'
+    }else if(score<70) {
+        return 'เกรด C'
+    }else if(score<80) {
+        return 'เกรด B'
+    } else 
+       return 'เกรด A'
+    }
+        
+    
+
+console.log(grade(50,100,100)) 
+
+
+
+
+
+
+
+
+
+
+   
